994efc5901df5099f904941ba9a1b035295a1496
ubuntu18-04 <amitba65@gmail.com>
2020-01-08 12:44:47 -0800

    adding a call to hde_32 functions

--- injector_by_addr/main.cpp
+++ injector_by_addr/main.cpp
@@ -7,6 +7,11 @@
 #include <stdlib.h>
 #include <sstream>
 #include <iomanip>
+#include <sys/ptrace.h>
+#include<unistd.h>
+#include<sys/wait.h>
+
+#include "hde32.h"
 
 using namespace std;
 using LoggerFunctionPtr = void (*)(std::string);
@@ -54,16 +59,51 @@ public:
 		_logger("Indicators has been initiated.");
 	}
 	
+	~Injector()
+	{
+		_logger("Been there done that.");
+	}
+	
+	bool inject_to_libc_open()const
+	{
+		ptrace(PTRACE_ATTACH, _targetPid, NULL, NULL);
+		wait(NULL);
+		_logger("cheking trampoline length"); 	
+		unsigned int _N_bytes_to_back_up = this->get_how_many_bytes_to_save();
+		_logger("trampoline length is " + _N_bytes_to_back_up);
+		
+		ptrace(PTRACE_DETACH, _targetPid, NULL, NULL);
+		return true;
+	}
+	
+	
+	
 private:
 	const unsigned long _targetPid;
 	const uint32_t _injection_addr;
 	LoggerFunctionPtr _logger=nullptr;
 	char _original_code_and_jnp_to_pus_N[25];
 
-	int get_how_many_bytes_to_save(unsigned long injection_addr)
+	int get_how_many_bytes_to_save()const
 	{
-		(void)injection_addr;
-		return 0;
+		const void* functionAddress = reinterpret_cast<void*>(_injection_addr);
+		unsigned int trampolineLength = 0;
+		hde32s disam;
+		 
+		if(!functionAddress)
+		 return 0;
+		 
+		//disassemble length of each instruction, until we have 5 or more bytes worth
+		while(trampolineLength < 5)
+		{
+			_logger("trampolineLength is " + (trampolineLength));
+			_logger("instractionPointer is " + (trampolineLength));
+		 	void* instructionPointer = (void*)((unsigned int)functionAddress + trampolineLength);
+		 	trampolineLength += hde32_disasm(instructionPointer, &disam);
+		}
+		_logger("after while trampolineLength is " + (trampolineLength));
+		return trampolineLength;
+			
 	}
 
 };
@@ -82,9 +122,11 @@ MyArgs validateArgs(int argc, char* argv[])
 
 int main(int argc, char* argv[])
 {
+	cout << "Run Me With sudo (!) " << endl;
 	MyArgs args = validateArgs(argc, argv);
 	Injector nurse(args.pid, args.injection_addr, logerToStdOut);
-	std::cout << "Done!! \n";
+	nurse.inject_to_libc_open();
+	nurse.inject_to_libc_open();
 }
 
 
