5ccf1fcfe75036471be508a2b301e1730cd5beb9
ubuntu18-04 <amitba65@gmail.com>
2020-01-17 19:05:17 +0200

    adding trial for loading the malicious shared object(.so) file that will perform the hooking

--- target_apps/main.cpp
+++ target_apps/main.cpp
@@ -3,11 +3,9 @@
 #include <fstream>
 #include <string>
 #include <unistd.h>
-
-
-
-
-  #include <stdlib.h>
+#include <stdlib.h>
+#include "dlfcn.h"
+#include <stdio.h>
 
 
 using namespace std;
@@ -21,6 +19,8 @@ int main(int argc, char *argv[])
 	else
 	{
 		cout << "here is the files content: \n";
+		void * library_handler = dlopen("/home/amit/Desktop/Projects/Injector/injector_by_addr/lib_proxy_open_inject.so", RTLD_NOW);
+		
 		while(true)
 		{
 			ifstream fileToRead(argv[1]);
@@ -32,6 +32,7 @@ int main(int argc, char *argv[])
 					cout << line << endl;
 				}
 				fileToRead.close();
+				return 0;
 				usleep(1000);
 				system("clear");
 			}
